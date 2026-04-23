d898a1839b2561b6a60bac1932e848e6d72f4117
Rohit kumar singh <singhrajput42369@gmail.com>
2026-04-23 18:10:21 +0530

    Update login API endpoint to production URL

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
 
   const login = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://web-applications-backend.onrender.com/api/auth/login", {
         email, password
       });
 
